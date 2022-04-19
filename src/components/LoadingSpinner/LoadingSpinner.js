f1bdc88b8f43546c2a6d96e49f6d5642b2ac12ed
sahlabdelrahman <sahlabdelrahman07@gmail.com>
2022-04-19 13:59:47 -0700

    feat: Getting & displaying images from backend's file as a grid

---
+++ src/components/LoadingSpinner/LoadingSpinner.js
@@ -0,0 +1,11 @@
+import "../../css/LoadingSpinner/LoadingSpinner.css";
+
+const LoadingSpinner = () => {
+  return (
+    <div className="spinner-container">
+      <div className="loading-spinner"></div>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
