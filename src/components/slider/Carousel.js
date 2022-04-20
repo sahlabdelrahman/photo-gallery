af00392ef20aa545d81d7065c4d626f65d209a50
sahlabdelrahman <sahlabdelrahman07@gmail.com>
2022-04-20 08:02:18 -0700

    feat: Making carousel swipeable on mobile devices

--- src/components/slider/Carousel.js
+++ src/components/slider/Carousel.js
@@ -1,4 +1,7 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useSwipeable } from "react-swipeable";
+
+import { updateActiveImage } from "../../store/actions/actions";
 
 import CarouselItem from "./CarouselItem/CarouselItem";
 
@@ -6,9 +9,15 @@ import "../../css/slider/Carousel.css";
 
 const Carousel = () => {
   const { images, activeImage } = useSelector((state) => state.images);
+  const dispatch = useDispatch();
+
+  const handlers = useSwipeable({
+    onSwipedLeft: () => dispatch(updateActiveImage(activeImage + 1)),
+    onSwipedRight: () => dispatch(updateActiveImage(activeImage - 1)),
+  });
 
   return (
-    <div className="carousel">
+    <div {...handlers} className="carousel">
       <div
         className="inner"
         style={{ transform: `translateX(-${activeImage * 100}%)` }}
